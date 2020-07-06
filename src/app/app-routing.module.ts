6f92d2335ee91356771758aac780e31944f6711b
CesarHortua <66738964+CesarHortua@users.noreply.github.com>
2020-07-06 17:17:45 -0500

    6-7-2020
    Se cambió LoginPage y se agregaron íconos circualres.
    Próximo paso; direccionamiento.
    
    Se guarda copia enviando correo a Viviana.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
 const alertCtrl = document.querySelector('ion-alert-controller');
 
 const routes: Routes = [
